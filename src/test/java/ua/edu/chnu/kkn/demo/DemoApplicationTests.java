30482d028361553b5474055ffc872611104154f6
Edgar Vatamanitsa <e.vatamanitsa@chnu.edu.ua>
2024-04-25 00:09:01 +0300

    [6][4] Fixed DemoApplicationTests by adding test containers.

--- src/test/java/ua/edu/chnu/kkn/demo/DemoApplicationTests.java
+++ src/test/java/ua/edu/chnu/kkn/demo/DemoApplicationTests.java
@@ -1,9 +1,13 @@
 package ua.edu.chnu.kkn.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("spring-boot-test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class DemoApplicationTests {
 
 	@Test
