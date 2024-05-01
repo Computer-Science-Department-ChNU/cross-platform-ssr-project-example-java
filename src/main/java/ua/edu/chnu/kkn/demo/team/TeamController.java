6239f82348bdc9e5fa12a33700e2dbcfe8877d7d
Edgar Vatamanitsa <e.vatamanitsa@chnu.edu.ua>
2024-05-01 16:24:53 +0300

    [7][3] Added config property for items per page. Modified team controller to retrive team members pageable. Modified team list template to display first 10th members.

--- src/main/java/ua/edu/chnu/kkn/demo/team/TeamController.java
+++ src/main/java/ua/edu/chnu/kkn/demo/team/TeamController.java
@@ -1,16 +1,28 @@
 package ua.edu.chnu.kkn.demo.team;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.SortDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ua.edu.chnu.kkn.demo.team.teammember.TeamMemberService;
 
 @Controller
 @RequestMapping("/team")
 public class TeamController {
 
+    private final TeamMemberService teamMemberService;
+
+    public TeamController(TeamMemberService teamMemberService) {
+        this.teamMemberService = teamMemberService;
+    }
+
     @GetMapping
-    public String team(Model model) {
+    public String team(Model model, @SortDefault.SortDefaults({ @SortDefault("teamMemberName.lastName"),
+            @SortDefault("teamMemberName.firstName")}) Pageable pageable) {
+        model.addAttribute( "title", "Team Members");
+        model.addAttribute("teamMembers", teamMemberService.getTeamMembers(pageable));
         return "team/list";
     }
 }
